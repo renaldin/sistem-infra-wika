1981108476676c17c779bd5a38299cd1bf247132
pritanurr <rifdahprita@Gmail.com>
2023-10-27 12:42:11 +0700

    edit jadi dropdown

--- resources/views/admin/projek/form.blade.php
+++ resources/views/admin/projek/form.blade.php
@@ -34,31 +34,49 @@
                         </div>
                         <div class="form-group col-md-6">
                             <label class="form-label" for="tipe_konstruksi">Tipe Konstruksi</label>
-                            <input type="text" class="form-control @error('tipe_konstruksi') is-invalid @enderror" id="tipe_konstruksi" name="tipe_konstruksi" value="@if($form === 'Tambah'){{ old('tipe_konstruksi') }}@elseif($form === 'Edit' || $form === 'Detail'){{$detail->tipe_konstruksi}}@endif"  @if($form === 'Detail') disabled @endif placeholder="Masukkan Nomor tipe_konstruksi">
+                            <select class="form-control @error('tipe_konstruksi') is-invalid @enderror" id="tipe_konstruksi" name="tipe_konstruksi" @if($form === 'Detail') disabled @endif>
+                                <option value="" selected disabled>-- Pilih Tipe Konstruksi --</option>
+                                <option value="Road & Bridge" @if($form === 'Tambah' && old('tipe_konstruksi') === 'Road & Bridge') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->tipe_konstruksi === 'Road & Bridge') selected @endif>Road & Bridge</option>
+                                <option value="Water Resource" @if($form === 'Tambah' && old('tipe_konstruksi') === 'Water Resource') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->tipe_konstruksi === 'Water Resource') selected @endif>Water Resource</option>
+                                <option value="Dredging & Land Clearing" @if($form === 'Tambah' && old('tipe_konstruksi') === 'Dredging & Land Clearing') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->tipe_konstruksi === 'Dredging & Land Clearing') selected @endif>Dredging & Land Clearing</option>
+                            </select>
                             @error('tipe_konstruksi')
                                 <div class="invalid-feedback">
-                                {{ $message }}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
+                        
                         <div class="form-group col-md-6">
                             <label class="form-label" for="prioritas">Prioritas</label>
-                            <input type="text" class="form-control @error('prioritas') is-invalid @enderror" id="prioritas" name="prioritas" value="@if($form === 'Tambah'){{ old('prioritas') }}@elseif($form === 'Edit' || $form === 'Detail'){{$detail->prioritas}}@endif"  @if($form === 'Detail') disabled @endif placeholder="Masukkan Nomor prioritas">
+                            <select class="form-control @error('prioritas') is-invalid @enderror" id="prioritas" name="prioritas" @if($form === 'Detail') disabled @endif>
+                                <option value="" selected disabled>-- Pilih Prioritas --</option>
+                                <option value="Prioritas 1" @if($form === 'Tambah' && old('prioritas') === 'Prioritas 1') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->prioritas === 'Prioritas 1') selected @endif>Prioritas 1</option>
+                                <option value="Prioritas 2" @if($form === 'Tambah' && old('prioritas') === 'Prioritas 2') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->prioritas === 'Prioritas 2') selected @endif>Prioritas 2</option>
+                                <option value="Prioritas 3" @if($form === 'Tambah' && old('prioritas') === 'Prioritas 3') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->prioritas === 'Prioritas 3') selected @endif>Prioritas 3</option>
+                                <option value="Bukan Prioritas" @if($form === 'Tambah' && old('prioritas') === 'Bukan Prioritas') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->prioritas === 'Bukan Prioritas') selected @endif>Bukan Prioritas</option>
+                            </select>
                             @error('prioritas')
                                 <div class="invalid-feedback">
-                                {{ $message }}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
                         <div class="form-group col-md-6">
                             <label class="form-label" for="status">Status</label>
-                            <input type="text" class="form-control @error('status') is-invalid @enderror" id="status" name="status" value="@if($form === 'Tambah'){{ old('status') }}@elseif($form === 'Edit' || $form === 'Detail'){{$detail->status}}@endif"  @if($form === 'Detail') disabled @endif placeholder="Masukkan Nomor status">
+                            <select class="form-control @error('status') is-invalid @enderror" id="status" name="status" @if($form === 'Detail') disabled @endif>
+                                <option value="" selected disabled>-- Pilih Status --</option>
+                                <option value="Proyek Besar" @if($form === 'Tambah' && old('status') === 'Proyek Besar') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->status === 'Proyek Besar') selected @endif>Proyek Besar</option>
+                                <option value="Proyek Menengah" @if($form === 'Tambah' && old('status') === 'Proyek Menengah') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->status === 'Proyek Menengah') selected @endif>Proyek Menengah</option>
+                                <option value="Proyek Kecil" @if($form === 'Tambah' && old('status') === 'Proyek Kecil') selected @elseif($form === 'Edit' || $form === 'Detail' && $detail->status === 'Proyek Kecil') selected @endif>Proyek Kecil</option>
+                            </select>
                             @error('status')
                                 <div class="invalid-feedback">
-                                {{ $message }}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
+                        
                         <div class="form-group col-md-6">
                             <label class="form-label" for="coordinat">Coordinat</label>
                             <input type="text" class="form-control @error('coordinat') is-invalid @enderror" id="coordinat" name="coordinat" value="@if($form === 'Tambah'){{ old('coordinat') }}@elseif($form === 'Edit' || $form === 'Detail'){{$detail->coordinat}}@endif"  @if($form === 'Detail') disabled @endif placeholder="Masukkan Nomor coordinat">
@@ -72,7 +90,7 @@
                             <label class="form-label" for="role">Kesie Eng</label>
                             <select name="id_kesie_eng" id="id_kesie_eng" class="selectpicker form-control @error('id_kesie_eng') is-invalid @enderror" data-style="py-0" @if($form === 'Detail') disabled @endif>
                                 @if ($form === 'Tambah')
-                                    <option value="">-- Pilih --</option>    
+                                    <option value="" selected disabled>-- Pilih --</option>    
                                 @elseif ($form === 'Edit' || $form === 'Detail')
                                     <option value="{{$detail->id_kesie_eng}}">{{$detail->nama_user}}</option>
                                 @endif
